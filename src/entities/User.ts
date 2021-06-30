2d89546fb1c3c4491a62e234a9772a06d5906128
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-06-30 00:12:47 -0300

    entity user

---
+++ src/entities/User.ts
@@ -0,0 +1,29 @@
+import {Column, Entity, PrimaryColumn, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity("users")
+export class User {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+}
