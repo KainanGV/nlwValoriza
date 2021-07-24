fb87999c43b6200721374c80127f924bad9e8d7a
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-07-24 20:43:58 -0300

    add camadas de usuarios

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import "reflect-metadata";
 import express from 'express';
+import { router } from './routes';
 
 import './database';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 /**
  * Tipos de parâmetros
