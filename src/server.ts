b6fe35bf3c56e1f297d88507b002c4d5d6e79d48
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-07-26 22:34:12 -0300

    tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import "reflect-metadata";
-import express from 'express';
-import { router } from './routes';
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import { router } from "./routes";
 
-import './database';
+import "./database";
 
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use(
+  ({ message }: Error, request: Request, response: Response, next: NextFunction) => {
+    if (message) {
+      return response.status(400).json({ error: message });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+    })
+  }
+);
 
 /**
  * Tipos de parâmetros
