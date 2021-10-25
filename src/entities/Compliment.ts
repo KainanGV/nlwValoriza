e7ba75d78b292d22f8ba08394172cac96988d176
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-10-24 21:33:35 -0300

    add relation Compliments

--- src/entities/Compliment.ts
+++ src/entities/Compliment.ts
@@ -1,5 +1,7 @@
-import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
-import { v4 as uuid } from 'uuid'; 
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, JoinColumn, ManyToOne } from "typeorm";
+import { v4 as uuid } from 'uuid';
+import Tag from "./Tag";
+import { User } from "./User";
 
 @Entity("compliments")
 class Compliment {
@@ -10,11 +12,23 @@ class Compliment {
     @Column()
     user_sender: string;
 
+    @JoinColumn({ name: 'user_sender' })
+    @ManyToOne(() => User)
+    userSender: User;
+
     @Column()
     user_receiver: string;
 
+    @JoinColumn({ name: 'user_receiver' })
+    @ManyToOne(() => User)
+    userReceiver: User;
+
     @Column()
     tag_id: string;
+
+    @JoinColumn({ name: "tag_id" })
+    @ManyToOne(() => Tag)
+    tag: Tag;
 
     @Column()
     message: string;
@@ -23,7 +37,7 @@ class Compliment {
     created_at: Date;
 
     constructor() {
-        if(!this.id) {
+        if (!this.id) {
             this.id = uuid();
         }
     }
